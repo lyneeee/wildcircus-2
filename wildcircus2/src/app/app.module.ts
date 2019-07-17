cd2c58fe558fbb40ad887d4f8f50fa1323bab4dc
Lyneeee <namiss.lyne@gmail.com>
2019-07-17 10:02:15 +0200

    add billets

--- wildcircus2/src/app/app.module.ts
+++ wildcircus2/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { TicketsComponent } from './tickets/tickets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    HomepageComponent
+    HomepageComponent,
+    TicketsComponent
   ],
   imports: [
     BrowserModule
