4973cf0c82a191bc3e47f1a8839d70d6356b4365
Lyneeee <namiss.lyne@gmail.com>
2019-07-17 15:51:03 +0200

    add contact

--- wildcircus2/src/app/app.module.ts
+++ wildcircus2/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { NavbarComponent } from './header/navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { TicketsComponent } from './tickets/tickets.component';
 import { PhotosComponent } from './photos/photos.component';
+import { NavbarrComponent } from './navbarr/navbarr.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent,
     HomepageComponent,
     TicketsComponent,
-    PhotosComponent
+    PhotosComponent,
+    NavbarrComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- wildcircus2/src/app/contact/contact.component.spec.ts
+++ wildcircus2/src/app/contact/contact.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponent } from './navbar.component';
+import { ContactComponent } from './contact.component';
 
-describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+describe('ContactComponent', () => {
+  let component: ContactComponent;
+  let fixture: ComponentFixture<ContactComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ ContactComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(ContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
